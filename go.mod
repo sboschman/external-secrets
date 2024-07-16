ca30053b090f9350aaf07d14a6f62654934bf50d
Sverre Boschman <1142569+sboschman@users.noreply.github.com>
2024-07-16 11:53:31 +0200

    feat(provider/keyhub): implement GetSecretMap and GetAllSecrets

--- go.mod
+++ go.mod
@@ -82,6 +82,7 @@ require (
 	github.com/keeper-security/secrets-manager-go/core v1.6.3
 	github.com/lestrrat-go/jwx/v2 v2.0.21
 	github.com/maxbrunsfeld/counterfeiter/v6 v6.8.1
+	github.com/microsoft/kiota-abstractions-go v1.6.0
 	github.com/passbolt/go-passbolt v0.7.0
 	github.com/pulumi/esc v0.8.3
 	github.com/scaleway/scaleway-sdk-go v1.0.0-beta.27
@@ -154,7 +155,6 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/microsoft/kiota-abstractions-go v1.6.0 // indirect
 	github.com/microsoft/kiota-http-go v1.4.1 // indirect
 	github.com/microsoft/kiota-serialization-json-go v1.0.7 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
--- pkg/provider/keyhub/client.go
+++ pkg/provider/keyhub/client.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"encoding/base64"
 	"errors"
+	"fmt"
 	"strconv"
 	"time"
 
@@ -29,8 +30,14 @@ import (
 
 	esv1beta1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
 	"github.com/external-secrets/external-secrets/pkg/cache"
+	"github.com/external-secrets/external-secrets/pkg/find"
 	khClient "github.com/external-secrets/external-secrets/pkg/provider/keyhub/client"
 	"github.com/external-secrets/external-secrets/pkg/provider/keyhub/util"
+)
+
+const (
+	errInvalidProperty        = "invalid property '%s'"
+	errUnexpectedFindOperator = "unexpected find operator"
 )
 
 var (
@@ -71,38 +78,28 @@ func NewClient(ctx context.Context, cfg *esv1beta1.KeyHubProvider, kube kubeClie
 }
 
 func (c *client) GetSecret(ctx context.Context, ref esv1beta1.ExternalSecretDataRemoteRef) ([]byte, error) {
-	logger.V(1).Info("fetching secret", "key", ref.Key, "property", ref.Property)
+	if ref.Version != "" {
+		return nil, errors.New("specifying a version is not supported by KeyHub")
+	}
 
-	record, err := c.getVaultRecord(ctx, ref)
+	property := ref.Property
+	if property == "" {
+		property = "password"
+	}
+
+	logger.V(1).Info("fetching secret", "key", ref.Key, "property", property)
+
+	record, err := c.getVaultRecord(ctx, ref.Key)
 	if err != nil {
 		return nil, err
 	}
 
-	secret := record.GetAdditionalObjects().GetSecret()
-	audit := record.GetAdditionalObjects().GetAudit()
-
-	switch ref.Property {
-	case "name":
-		return []byte(*record.GetName()), nil
-	case "color":
-		return []byte(record.GetColor().String()), nil
-	case "link":
-		return []byte(*record.GetUrl()), nil
-	case "username":
-		return []byte(*record.GetUsername()), nil
-	case "filename":
-		return []byte(*record.GetFilename()), nil
-	case "file":
-		return base64.StdEncoding.DecodeString(*secret.GetFile())
-	case "enddate":
-		return []byte(record.GetEndDate().String()), nil
-	case "comment":
-		return []byte(*secret.GetComment()), nil
-	case "lastModifiedAt":
-		return []byte(audit.GetLastModifiedAt().UTC().Format(time.RFC3339)), nil
-	default:
-		return []byte(*secret.GetPassword()), nil
+	values, err := c.getVaultRecordProperties(record, property)
+	if err != nil {
+		return nil, err
 	}
+
+	return values[property], nil
 }
 
 func (c *client) PushSecret(_ context.Context, _ *corev1.Secret, _ esv1beta1.PushSecretData) error {
@@ -122,18 +119,96 @@ func (c *client) Validate() (esv1beta1.ValidationResult, error) {
 }
 
 func (c *client) GetSecretMap(ctx context.Context, ref esv1beta1.ExternalSecretDataRemoteRef) (map[string][]byte, error) {
-	return nil, errors.New("not implemented")
+	if ref.Version != "" {
+		return nil, errors.New("specifying a version is not supported by KeyHub")
+	}
+
+	logger.V(1).Info("fetching property map for secret", "key", ref.Key, "property", ref.Property)
+
+	record, err := c.getVaultRecord(ctx, ref.Key)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.getVaultRecordProperties(record, ref.Property)
 }
 
-func (c *client) GetAllSecrets(_ context.Context, _ esv1beta1.ExternalSecretFind) (map[string][]byte, error) {
-	// TODO (maybe):
-	//   - path: <group uuid>
-	//   - name: <regex obv vault record name>
-	//   - tags
-	//       group: <group uuid> (of via path?)
-	//       color: <color> (alle secrets met kleurtje x, nuttig???)
+func (c *client) GetAllSecrets(ctx context.Context, ref esv1beta1.ExternalSecretFind) (map[string][]byte, error) {
+	secrets, err := c.listSecrets(ctx, c.newVaultRecordFilter(ref))
+	if err != nil {
+		return nil, err
+	}
 
-	return nil, errors.New("not implemented")
+	secretData := make(map[string][]byte, len(secrets)*2)
+	for _, s := range secrets {
+		record, err := c.getVaultRecord(ctx, *s.GetUuid())
+		if err != nil {
+			return nil, err
+		}
+
+		recordProperties, err := c.getVaultRecordProperties(record, "")
+		if err != nil {
+			return nil, err
+		}
+
+		isSingleProperty := len(recordProperties) == 1
+		for propertyName, property := range recordProperties {
+			key := *record.GetName()
+			if !isSingleProperty {
+				key = fmt.Sprintf("%s/%s", key, propertyName)
+			}
+			secretData[key] = property
+		}
+	}
+
+	return secretData, nil
+}
+
+func (c *client) newVaultRecordFilter(ref esv1beta1.ExternalSecretFind) khClient.VaultRecordFilter {
+	filter := khClient.VaultRecordFilter{}
+
+	if ref.Path != nil {
+		filter.Group = *ref.Path
+	}
+
+	if ref.Name != nil {
+		filter.Name = ref.Name
+	}
+
+	// TODO: map tags to VaultRecordFilter
+	// for k, v := range ref.Tags {
+	// }
+
+	return filter
+}
+
+func (c *client) listSecrets(ctx context.Context, filter khClient.VaultRecordFilter) ([]keyhubmodels.VaultVaultRecordable, error) {
+	potentialSecrets, err := c.client.GetVaultRecordMetadata(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	if filter.Name != nil {
+		return c.filterSecretsByName(potentialSecrets, *filter.Name)
+	}
+
+	return potentialSecrets, nil
+}
+
+func (c *client) filterSecretsByName(secrets []keyhubmodels.VaultVaultRecordable, name esv1beta1.FindName) ([]keyhubmodels.VaultVaultRecordable, error) {
+	matcher, err := find.New(name)
+	if err != nil {
+		return nil, err
+	}
+
+	filtered := make([]keyhubmodels.VaultVaultRecordable, 0, len(secrets))
+	for _, r := range secrets {
+		if matcher.MatchName(*r.GetName()) {
+			filtered = append(filtered, r)
+		}
+	}
+
+	return filtered, nil
 }
 
 func (c *client) Close(context.Context) error {
@@ -141,13 +216,9 @@ func (c *client) Close(context.Context) error {
 }
 
 // getVaultRecord retrieves the vault record referenced by ref from the KeyHub API.
-func (c *client) getVaultRecord(ctx context.Context, ref esv1beta1.ExternalSecretDataRemoteRef) (keyhubmodels.VaultVaultRecordable, error) {
-	if ref.Version != "" {
-		return nil, errors.New("specifying a version is not supported by KeyHub")
-	}
-
+func (c *client) getVaultRecord(ctx context.Context, uuid string) (keyhubmodels.VaultVaultRecordable, error) {
 	key := cache.Key{
-		Name:      ref.Key,
+		Name:      uuid,
 		Namespace: "",
 		Kind:      "",
 	}
@@ -155,42 +226,114 @@ func (c *client) getVaultRecord(ctx context.Context, ref esv1beta1.ExternalSecre
 	// Only check 'version' (KeyHub lastModifiedAt field) if key exists in cache
 	ok := c.recordCache.Contains(key)
 	if ok {
-		lastModifiedAt, err := c.client.GetVaultRecordLastModifiedAt(ctx, ref.Key)
+		lastModifiedAt, err := c.client.GetVaultRecordLastModifiedAt(ctx, uuid)
 		if err != nil {
 			return nil, err
 		}
 
-		logger.V(1).Info("lookup secret from cache", "key", ref.Key, "lastModifiedAt", lastModifiedAt.String())
+		logger.V(1).Info("lookup secret from cache", "key", uuid, "lastModifiedAt", lastModifiedAt.String())
 		record, ok := c.recordCache.Get(strconv.FormatInt(lastModifiedAt.Unix(), 10), key)
 		if ok {
 			return record, nil
 		}
 	}
 
-	c.useMu.LockKey(ref.Key)
+	c.useMu.LockKey(uuid)
 	defer func() {
-		_ = c.useMu.UnlockKey(ref.Key)
+		_ = c.useMu.UnlockKey(uuid)
 	}()
 
 	ok = c.recordCache.Contains(key)
 	if ok {
-		lastModifiedAt, err := c.client.GetVaultRecordLastModifiedAt(ctx, ref.Key)
+		lastModifiedAt, err := c.client.GetVaultRecordLastModifiedAt(ctx, uuid)
 		if err != nil {
 			return nil, err
 		}
 
-		logger.V(1).Info("lookup secret from cache", "key", ref.Key, "lastModifiedAt", lastModifiedAt.String())
+		logger.V(1).Info("lookup secret from cache", "key", uuid, "lastModifiedAt", lastModifiedAt.String())
 		record, ok := c.recordCache.Get(strconv.FormatInt(lastModifiedAt.Unix(), 10), key)
 		if ok {
 			return record, nil
 		}
 	}
 
-	record, err := c.client.GetVaultRecord(ctx, ref.Key)
+	record, err := c.client.GetVaultRecord(ctx, uuid)
 	if err != nil {
 		return nil, err
 	}
-	logger.V(1).Info("caching version", "key", ref.Key, "lastModifiedAt", record.GetAdditionalObjects().GetAudit().GetLastModifiedAt())
+	logger.V(1).Info("caching version", "key", uuid, "lastModifiedAt", record.GetAdditionalObjects().GetAudit().GetLastModifiedAt())
 	c.recordCache.Add(strconv.FormatInt(record.GetAdditionalObjects().GetAudit().GetLastModifiedAt().Unix(), 10), key, record)
 	return record, nil
+}
+
+func (c *client) getVaultRecordProperties(record keyhubmodels.VaultVaultRecordable, property string) (map[string][]byte, error) {
+	data := make(map[string][]byte, 12)
+
+	fetchAll := property == ""
+	secret := record.GetAdditionalObjects().GetSecret()
+	audit := record.GetAdditionalObjects().GetAudit()
+
+	if property == "name" {
+		data["name"] = []byte(*record.GetName())
+	}
+	if property == "color" {
+		if record.GetColor() != nil {
+			data["color"] = []byte(record.GetColor().String())
+		} else {
+			data["color"] = []byte("")
+		}
+	}
+	if fetchAll || property == "link" {
+		if record.GetUrl() != nil {
+			data["link"] = []byte(*record.GetUrl())
+		} else if !fetchAll {
+			data["link"] = []byte("")
+		}
+	}
+	if fetchAll || property == "username" {
+		if record.GetUsername() != nil {
+			data["username"] = []byte(*record.GetUsername())
+		} else if !fetchAll {
+			data["username"] = []byte("")
+		}
+	}
+	if fetchAll || property == "password" {
+		if secret.GetPassword() != nil {
+			data["password"] = []byte(*secret.GetPassword())
+		} else {
+			data["password"] = []byte("")
+		}
+	}
+	if property == "filename" {
+		if record.GetFilename() != nil {
+			data["filename"] = []byte(*record.GetFilename())
+		} else {
+			data["filename"] = []byte("")
+		}
+	}
+	if (fetchAll || property == "file") && secret.GetFile() != nil {
+		value, err := base64.StdEncoding.DecodeString(*secret.GetFile())
+		if err != nil {
+			return nil, err
+		}
+		data["file"] = value
+	}
+
+	if (property == "enddate") && record.GetEndDate() != nil {
+		data["enddate"] = []byte(record.GetEndDate().String())
+	}
+	if (property == "comment") && secret.GetComment() != nil {
+		data["comment"] = []byte(*secret.GetComment())
+	}
+	if (property == "lastModifiedBy") && audit.GetLastModifiedBy() != nil {
+		data["lastModifiedBy"] = []byte(*audit.GetLastModifiedBy())
+	}
+	if (property == "lastModifiedAt") && audit.GetLastModifiedAt() != nil {
+		data["lastModifiedAt"] = []byte(audit.GetLastModifiedAt().UTC().Format(time.RFC3339))
+	}
+	if len(data) == 0 {
+		return nil, fmt.Errorf(errInvalidProperty, property)
+	}
+
+	return data, nil
 }
